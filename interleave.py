6f3b581a6289d47053af8c1d22922aa7e9e13042
user <yardenge@edu.hac.ac.il>
2022-04-21 10:43:45 +0300

    fixed documentation

--- interleave.py
+++ interleave.py
@@ -1,4 +1,7 @@
 def interleave(*iter_param):
+    # Returns interleave list of 3 given objects.
+    # Arguments: iter_param - undefined number of objects. can be tuple,list,string.
+
     return [val for obj in zip(*iter_param) for val in obj]
 
 
--- interliave-gen.py
+++ interliave-gen.py
@@ -4,6 +4,8 @@ This is the generator version to interleave.
 
 
 def interleave(*iter_param):
+    # Returns interleave list of 3 given objects.
+    # Arguments: iter_param - undefined number of objects. can be tuple,list,string.
     for tup in zip(*iter_param):
         yield list(tup)
 
